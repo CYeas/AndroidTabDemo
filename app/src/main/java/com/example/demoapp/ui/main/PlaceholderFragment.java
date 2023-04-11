52f076b9408e9cc223dc40db4b58bfc21836daf1
cyeas <me@chenyea.com>
2023-04-11 20:28:46 +0800

    feat: init repo

---
+++ app/src/main/java/com/example/demoapp/ui/main/PlaceholderFragment.java
@@ -0,0 +1,67 @@
+package com.example.demoapp.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.demoapp.R;
+import com.example.demoapp.databinding.FragmentMainBinding;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+    private FragmentMainBinding binding;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        WebView webView = binding.sectionLabel;
+        webView.loadUrl("https://qshv92.csb.app/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
